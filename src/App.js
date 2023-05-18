55af909c506c13581f37237728f45734ac7519eb
hvsp2704 <hvsp2704@gmail.com>
2023-05-19 02:21:40 +0530

    adding all files to the repo

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+var axios = require('axios');
+
+
+
+function App() {
+  const [inputText, setInputText] = useState('');
+  const [jsonString, setJsonString] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleConvertToJson = async() => {
+    console.log("here");
+    try {
+
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      
+      var raw = JSON.stringify({
+        "sequence": `${inputText}`
+      });
+      
+      var requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+      
+      fetch("http://localhost:3002/sequence/", requestOptions)
+        .then(response => response.text())
+        .then(result => setJsonString((result)))
+        .catch(error => console.log('error', error));
+      
+    } catch (error) {
+      console.error('Error converting to JSON:', error);
+      setJsonString('error');
+    }
+  };
+
+  return (
+    <div>
+      <input type="text" value={inputText} onChange={handleInputChange} />
+      <button onClick={handleConvertToJson}>find</button>
+      <div>
+        <pre>{jsonString}</pre>
+      </div>
+    </div>
+  );
+}
+
+export default App;
